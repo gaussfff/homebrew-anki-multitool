0c2db33e30c0b90b6d1f1bb747cd158f28089040
Bohdan Sokolovskyi <sokol.chemist@gmail.com>
2025-07-24 15:34:13 +0300

    init project

---
+++ anki-multitool.rb
@@ -0,0 +1,13 @@
+class AnkiMultitool < Formula
+    desc "Just useful multitool for Anki users"
+    homepage "https://github.com/gaussfff/anki-multitool"
+    version "0.0.1"
+
+    on_macos do
+        # TODO: implement
+    end
+    
+    on_linux do
+        # TODO: implement
+    end
+end
